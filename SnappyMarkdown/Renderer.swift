384063f5027d2aef9bbfe545f98e6e7ba74e55bc
Alexander Guschin <alexguschin.work@gmail.com>
2018-01-17 23:06:43 +0300

    Add basic renderer

---
+++ SnappyMarkdown/Renderer.swift
@@ -0,0 +1,74 @@
+//
+//  Renderer.swift
+//  SnappyMarkdown
+//
+//  Created by Alexander Guschin on 17.01.2018.
+//  Copyright © 2018 Alexander Guschin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension NSAttributedString {
+    convenience init(string: String, attributes: Attributes) {
+        let baseFontDescriptor = UIFontDescriptor(name: attributes.stylesheet.fontFamily, size: attributes.fontSize)
+        var fontTraits = UIFontDescriptorSymbolicTraits()
+        if attributes.bold {
+            let boldFontTrait = UIFontDescriptorSymbolicTraits.traitBold
+            fontTraits.formUnion(boldFontTrait)
+        }
+        let fontDescriptor = baseFontDescriptor.withSymbolicTraits(fontTraits)!
+        let font = UIFont(descriptor: fontDescriptor, size: 0)
+        self.init(string: string, attributes: [NSAttributedStringKey.font: font])
+    }
+}
+
+struct Stylesheet {
+    let fontFamily: String
+}
+
+struct Attributes {
+    var fontSize: CGFloat
+    var bold: Bool
+    let stylesheet: Stylesheet
+
+    mutating func strong() {
+        bold = true
+    }
+}
+
+extension Block {
+    func render(_ attributes: Attributes) -> NSAttributedString {
+        switch self {
+        case let .paragraph(elements):
+            return elements.map { $0.render(attributes) }.joined()
+        default:
+            fatalError("not implemented")
+        }
+    }
+}
+
+extension Inline {
+    func render(_ attributes: Attributes) -> NSAttributedString {
+        var newAttributes = attributes
+        switch self {
+        case let .text(string):
+            return NSAttributedString(string: string, attributes: attributes)
+        case let .strong(children):
+            newAttributes.strong()
+            return children.map { $0.render(newAttributes) }.joined()
+        default:
+            fatalError("not implemented")
+        }
+    }
+}
+
+extension Array where Element == NSAttributedString {
+    func joined() -> NSAttributedString {
+        let r = NSMutableAttributedString()
+        for s in self {
+            r.append(s)
+        }
+        return r
+    }
+}
