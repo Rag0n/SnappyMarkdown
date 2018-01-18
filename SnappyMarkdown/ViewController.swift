38e86ee8e2c21915c43143a7e8b7190918982737
Alexander Guschin <alexguschin.work@gmail.com>
2018-01-18 22:30:46 +0300

    Setup testing view controller

--- SnappyMarkdown/ViewController.swift
+++ SnappyMarkdown/ViewController.swift
@@ -9,15 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private var resultLabel: UILabel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        stackView.addArrangedSubview(editor)
         let attributes = Attributes(fontSize: 16, bold: false, stylesheet: Stylesheet(fontFamily: "Helvetica"))
         let testString = "**Hello world** Hello world"
         let tree = Node(markdown: testString)!.elements
         let result = tree[0].render(attributes)
         resultLabel.attributedText = result
     }
-}
 
+    @IBOutlet private var resultLabel: UILabel!
+    @IBOutlet private var stackView: UIStackView!
+    private let editor: UITextView = {
+        let storage = MarkdownTextStorage()
+        let layoutManager = NSLayoutManager()
+        let container = NSTextContainer(size: CGSize(width: 200, height: CGFloat.greatestFiniteMagnitude))
+        container.widthTracksTextView = true
+        layoutManager.addTextContainer(container)
+        storage.addLayoutManager(layoutManager)
+        return UITextView(frame: CGRect.zero, textContainer: container)
+    }()
+}
